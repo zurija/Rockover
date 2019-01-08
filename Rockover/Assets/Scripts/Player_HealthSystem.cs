1b740ed1e132b579773f4760500cff94fe9d7e02
zurija <46446136+zurija@users.noreply.github.com>
2019-01-08 10:20:15 +0100

    x

--- Rockover/Assets/Scripts/Player_HealthSystem.cs
+++ Rockover/Assets/Scripts/Player_HealthSystem.cs
@@ -11,6 +11,7 @@ public class Player_HealthSystem : MonoBehaviour
     [SerializeField] public int MaxLives;
     private int FlamethrowerDamage;
     [SerializeField] Text HealthStatText;
+    
 
 
     // Use this for initialization
@@ -29,7 +30,7 @@ public class Player_HealthSystem : MonoBehaviour
         if (other.gameObject.CompareTag("Fire")) {
             CurHealth = CurHealth - FlamethrowerDamage;
         }
-       // anim.Play("Hurt");
+       
         Debug.Log(CurHealth);
         if (CurHealth == 0)
         {
@@ -37,7 +38,13 @@ public class Player_HealthSystem : MonoBehaviour
             CurHealth = MaxHealth;
             SetHealthText();
         }
-        
+        if (MaxLives == 0)
+        {
+            anim.SetBool("isDead", true);
+        } else
+        {
+            anim.SetBool("isDead", false);
+        }
         Debug.Log(MaxLives);
     }
    
