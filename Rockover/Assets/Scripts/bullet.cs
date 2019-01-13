9f55ec08e9ce08a25b4c4c307e272b3b7239b96d
zurija <46446136+zurija@users.noreply.github.com>
2019-01-13 17:01:55 +0100

    Schießen

--- Rockover/Assets/Scripts/bullet.cs
+++ Rockover/Assets/Scripts/bullet.cs
@@ -3,26 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class bullet : MonoBehaviour {
+
     public float speed = 20f;
-    public Rigidbody2D rb;
     public int damage = 40;
-    public GameObject impactEffect; 
+    public Rigidbody2D rb;
+   
 
-	// Use this for initialization
-	void Start () {
-        rb.velocity = transform.right * speed; 
-	}
-
-    private void OnTriggerEnter2D(Collider2D hitInfo)
+    // Use this for initialization
+    void Start()
     {
-        /*hitInfo.GetComponent<Paparazzi>();
-        if (Paparazzi != null)
-        {
-        Paparazzi.TakeDamage(damage);
-        }*/
-        Debug.Log(hitInfo);
-        Instantiate(impactEffect, transform.position, transform.rotation);
-        Destroy(gameObject);
+        rb.velocity = transform.right * speed;
     }
 
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Paparazzi Paparazzi = hitInfo.GetComponent<Paparazzi>();
+        if (Paparazzi != null)
+        {
+            Paparazzi.TakeDamage(damage);
+        }
+        Debug.Log(hitInfo);
+        Destroy(gameObject);
+    }
+    
 }
